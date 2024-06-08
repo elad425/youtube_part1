54df2af5eeaef8afb2b2b0fd96758c294330fc55
ItayBenDaniel <itaybendaniel>
2024-06-08 23:34:38 +0300

    first rough prototype of sign in screen and login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
+
 }
